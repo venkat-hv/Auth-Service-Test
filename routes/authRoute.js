50de85d5040b9ef742fcc52ee18cdb8700f23558
Sai Venkat <kondeti.venkat@herovired.com>
2025-05-15 14:20:04 +0530

    test

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
+const passport = require("../passport")
 
 router.get("/google", authController.handleGoogleAuth);
 router.get(
